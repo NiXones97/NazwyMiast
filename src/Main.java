004cecf593ffe561a0de73877def0df2b28f37b7
Kacper Szyjka <kacper.szyjka97@gmail.com>
2022-11-06 19:40:07 +0100

    end

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,13 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
 
+        /*
+        Napisz program, który będzie pobierał od użytkownika nazwy miast.
+        Program powinien pozwolić użytkownikowi na wprowadzenie dowolnej liczby,
+         a gdy ktoś wprowadzi znak  „ / ” przerwie wprowadzanie i wypisze komunikat „błędny znak”.
+         Na zakończenie wypisz liczbę miast.
+         */
+
         String name;
         Set<String> names = new HashSet<>();
         Scanner scan = new Scanner(System.in);
