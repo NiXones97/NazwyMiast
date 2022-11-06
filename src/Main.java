e323c4fda04854d20e77334ce89bcfcebcab96e5
Kacper Szyjka <kacper.szyjka97@gmail.com>
2022-11-06 19:39:05 +0100

    end

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String name;
+        Set<String> names = new HashSet<>();
+        Scanner scan = new Scanner(System.in);
+
+
+        do {
+
+            System.out.println("Podaj Miasto : ");
+            name = scan.next();
+
+            if (!name.equals("-"))
+                names.add(name);
+        }
+
+        while (!name.equals("/"));
+        {
+        System.out.println("Błędne dane !");}
+
+
+        System.out.println("Liczba podanych prawidłowo miast to : " + names.size());
+
     }
+
+
+
+
 }
